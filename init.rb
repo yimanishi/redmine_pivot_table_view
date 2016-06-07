cbd7bb6708e0ce24db7a762a06225bdafddd766d
root <root@ip-172-31-17-77.ap-northeast-1.compute.internal>
2016-06-07 17:02:56 +0900

    first release.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_pivot_table_view do
+  name 'Redmine Pivot Table View plugin'
+  author 'Y.Imanishi'
+  description 'pivot table view.'
+  version '0.0.1'
+  url 'https://github.com/yimanishi/redmine_pivot_table_view'
+  author_url 'https://github.com/yimanishi/redmine_pivot_table_view'
+end
